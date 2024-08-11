d28345e4e629c516047639b86e693043af6425b3
guypas <guypascal100@gmail.com>
2024-08-11 17:31:39 +0300

    extension works

--- src/components/urlForm.js
+++ src/components/urlForm.js
@@ -19,7 +19,6 @@ export default function UrlForm({changeStates, onSubmit, loading }) {
     <div className="form-container">
         <input ref={inputUrl} type="text" placeholder="Enter URL" />
         <button disabled={loading} onClick={handleSubmit}>{loading ? 'Loading...' : 'Check Now!'} </button>
-      {/* {!isValidUrl && <p className="error-message">Please enter a valid URL</p>}  */}
     </div>
   )
 }
